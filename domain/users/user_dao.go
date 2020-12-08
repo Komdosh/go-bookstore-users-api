930e420e5d617a7a4b5fb67dab4552a2931cff9d
Andrey Tabakov <komdosh@yandex.ru>
2020-12-08 11:46:34 +0300

    feat(sql): implement get user request

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -9,7 +9,10 @@ import (
 )
 
 const (
-	queryInsertUser = "INSERT INTO users_db.users(first_name, last_name, email, date_created) VALUES ($1, $2, $3, $4)  RETURNING id;"
+	queryInsertUser     = "INSERT INTO users_db.users(first_name, last_name, email, date_created) VALUES ($1, $2, $3, $4) RETURNING id;"
+	querySelectUserById = "SELECT id, first_name, last_name, email, date_created FROM users_db.users WHERE id = $1;"
+	errorNoRows         = "no rows in result set"
+	errorNotUnique      = "uindex"
 )
 
 var (
@@ -17,21 +20,19 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr {
-
-	if err := users_db.Client.Ping(); err != nil {
-		panic(err)
+	stmt, err := users_db.Client.Prepare(querySelectUserById)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
 
-	result := usersDB[user.Id]
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	if err := stmt.QueryRow(user.Id).Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+
+		if strings.Contains(err.Error(), errorNoRows) {
+			return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to get user with id: %d", user.Id))
 	}
-
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
 
 	return nil
 }
@@ -48,7 +49,7 @@ func (user *User) Save() *errors.RestErr {
 	var userId int64
 	err = stmt.QueryRow(user.FirstName, user.LastName, user.Email, user.DateCreated).Scan(&userId)
 	if err != nil {
-		if strings.Contains(err.Error(), "uindex") {
+		if strings.Contains(err.Error(), errorNotUnique) {
 			return errors.NewBadRequestError(err.Error())
 		}
 		return errors.NewInternalServerError(
