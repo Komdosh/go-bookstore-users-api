9783ca0dfa309ff40cde1b5c46dd66bf342f557d
Andrey Tabakov <komdosh@yandex.ru>
2021-01-08 12:55:30 +0300

    feat(datasource): add init.sql

---
+++ init.sql
@@ -0,0 +1,31 @@
+create user gousr with password 'gousr';
+
+create database gomicroservices
+    with owner gousr;
+
+
+-- switch to gomicroservices before run next lines
+
+create schema users_db;
+
+create table users_db.users
+(
+    id           bigserial    not null
+        constraint users_pk
+            primary key,
+    first_name   varchar(255),
+    last_name    varchar(255),
+    email        varchar(255) not null,
+    date_created timestamp,
+    password     varchar(32)  not null,
+    status       varchar(255) not null
+);
+
+alter table users_db.users
+    owner to gousr;
+
+create unique index users_id_uindex
+    on users_db.users (id);
+
+create unique index users_email_uindex
+    on users_db.users (email);
